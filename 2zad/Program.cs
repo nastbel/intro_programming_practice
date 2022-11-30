c9780aba0a8c434b92e8df80a19c99591048a759
Poopa <poopa@loopa.za>
2022-11-30 23:30:38 +0300

    Сделано задание2

---
+++ 2zad/Program.cs
@@ -0,0 +1,50 @@
+﻿int max = 0;
+int min = 0;
+int a = 5;
+int b = 7;
+if (a >= b) 
+{
+    max = a;
+    min = b;
+} 
+else {
+    max = b;
+    min = a;
+}
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
+
+
+a = 2;
+b = 10;
+if (a >= b) 
+{
+    max = a;
+    min = b;
+} 
+else {
+    max = b;
+    min = a;
+}
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
+
+a = -9;
+b = -3;
+if (a >= b) 
+{
+    max = a;
+    min = b;
+} 
+else {
+    max = b;
+    min = a;
+}
+Console.Write("max = ");
+Console.WriteLine(max);
+Console.Write("min = ");
+Console.WriteLine(min);
