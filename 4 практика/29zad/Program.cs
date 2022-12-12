6f73bdc1b814f2a33041f739af998fffc89f780c
Poopa <poopa@loopa.za>
2022-12-12 14:37:12 +0300

    4 практика изменена

--- 4 практика/29zad/Program.cs
+++ 4 практика/29zad/Program.cs
@@ -1,7 +1,14 @@
-﻿int [] arr = new int[8];
-Random rand = new Random();
-for (int i = 0; i < arr.Length; i++)
-{
-    arr[i] = rand.Next(0,555);
-}
+﻿int [] arr = Auto();
+
 System.Console.WriteLine(string.Join(", ", arr));
+
+int[] Auto()
+{
+    int [] arr = new int[8];
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(0,555);
+    }
+    return arr;
+}
